35c0ca17b05423e81ebd61d7bd4a58ae559a8953
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-15 18:43:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,21 +1,20 @@
-phrase = "It's-a me, Mario!"
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-dangerous="It's Dangerous To Go Alone! Take This."
 def mario
-  
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  phrase = "It's-a me, Mario!"
   puts status
 end
 
 def link
+  dangerous="It's Dangerous To Go Alone! Take This."
   puts dangerous
 end
 
 def all_phrases
-  puts phrase
-  puts status
-  puts dangerous
+  mario
+  toadstool
+  link
 end
